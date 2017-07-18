582044242046e2c2da62ee0c5a271a0039703d8e
praveen soni <praveensoni1992@gmail.com>
2017-07-19 00:09:21 +0530

    checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductCart } from '../models/product-cart';
 import * as Collections from 'typescript-collections';
+import { CustomerInfo } from "../models/customerInfo";
 
 
 import { ActivatedRoute, ParamMap } from '@angular/router';
@@ -15,21 +16,23 @@ import 'rxjs/add/operator/switchMap';
 @Component({
     selector: 'checkout',
     templateUrl: './checkout.component.html',
-    styleUrls:['./checkout.component.css'],
-    providers: [ProductService,CartService]
+    styleUrls: ['./checkout.component.css'],
+    providers: [ProductService, CartService]
 })
 
 export class CheckoutComponent implements OnInit {
     products: Product[] = new Array<Product>();
     cartProducts: ProductCart[];
-    totVarType : string[];
-    subTotal : number = 0;
+    totVarType: string[];
+    subTotal: number = 0;
     public alerts: any = [];
+    cusInfo: CustomerInfo = new CustomerInfo;
+    states: string[] = ['Andaman and Nicobar', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chattisgarh', 'Dadra and Nagar Haveli', 'Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Orissa', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'];
 
     public status: any = {
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
 
 
     constructor(
@@ -41,29 +44,42 @@ export class CheckoutComponent implements OnInit {
 
     getProducts(): void {
 
-      this.cartProducts = this.cartService.getAllItems();
-      for(let cartProduct of this.cartProducts){
-        this.productService.getProduct(cartProduct.id).then(product => {
-            cartProduct.product = product;
-            this.subTotal+= product.price*cartProduct.qty;
-            for(let variant of product.variants){
-                console.log(cartProduct.variantId);
-                if(variant.id==cartProduct.variantId){
-                    cartProduct.variantName = variant.variantName;
+        this.cartProducts = this.cartService.getAllItems();
+        this.subTotal = 0;
+        for (let cartProduct of this.cartProducts) {
+            this.productService.getProduct(cartProduct.id).then(product => {
+                cartProduct.product = product;
+                this.subTotal += product.price * cartProduct.qty;
+                for (let variant of product.variants) {
+                    console.log(cartProduct.variantId);
+                    if (variant.id == cartProduct.variantId) {
+                        cartProduct.variantName = variant.variantName;
+                    }
                 }
-            }
-        });
-        
-      }
+            });
+
+        }
     }
-removeItem(item: ProductCart) {
+    removeItem(item: ProductCart) {
         this.cartService.removeItem(item);
         window.location.reload();
     }
 
     ngOnInit(): void {
+        //this.getProducts();
+    }
+
+    getOrderInfo(): void {
         this.getProducts();
     }
+
+
+ 
+placeOrder() : boolean {
+    let orderPlaced :boolean = false;
+    console.log(JSON.stringify(this.cusInfo));
+     return orderPlaced;
+}
 
 
 
